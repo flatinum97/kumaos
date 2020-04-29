229a21864d1fe2e8c4d75cb6a9fd46aedcc403ca
grezar <grezar.dev@gmail.com>
2020-04-29 15:29:32 +0900

    Call C from assembly

---
+++ kmain.c
@@ -0,0 +1,15 @@
+#define FB_GREEN 2
+#define FB_DARK_GREY 8
+
+char *fb = (char *) 0x000B8000;
+
+void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
+{
+  fb[i] = c;
+  fb[i+1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
+}
+
+void kmain() {
+  fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
+  while(1);
+}
--- loader.s
+++ loader.s
@@ -1,9 +1,11 @@
 global loader
 
-MAGIC_NUMBER equ 0x1BADB002
-FLAGS        equ 0x0
-CHECKSUM     equ -MAGIC_NUMBER
+extern kmain
 
+MAGIC_NUMBER      equ 0x1BADB002
+FLAGS             equ 0x0
+CHECKSUM          equ -MAGIC_NUMBER
+KERNEL_STACK_SIZE equ 4096
 
 section .text:
 align 4
@@ -13,5 +15,10 @@ align 4
 
 loader:
     mov eax, 0xCAFEBABE
-.loop:
-    jmp .loop
+    mov esp, kernel_stack + KERNEL_STACK_SIZE
+    jmp kmain
+
+section .bss
+align 4
+kernel_stack:
+    resb KERNEL_STACK_SIZE
