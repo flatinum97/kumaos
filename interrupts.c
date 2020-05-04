5dd77c366092aa675396556c9c4c6770fc92d78d
grezar <grezar.dev@gmail.com>
2020-05-04 13:42:48 +0900

    Receive interrupt parameters on its handler

--- interrupts.c
+++ interrupts.c
@@ -1,6 +1,10 @@
+#include "interrupts.h"
 #include "stdio.h"
 
-void interrupt_handler() {
-  log("interrupt_handler()\n");
+void interrupt_handler(struct cpu_state cpu, uint32_t interrupt_number, uint32_t error_code) {
+  // Avoid unused parameter errors
+  (void) cpu;
+  (void) interrupt_number;
+  (void) error_code;
   return;
 }
--- interrupts.h
+++ interrupts.h
@@ -1,6 +1,19 @@
 #ifndef INCLUDE_INTERRUPTS_H
 #define INCLUDE_INTERRUPTS_H
 
-void interrupt_handler();
+#include "types.h"
+
+struct cpu_state {
+  uint32_t eax;
+  uint32_t ebx;
+  uint32_t ecx;
+  uint32_t edx;
+  uint32_t esi;
+  uint32_t edi;
+  uint32_t esp;
+  uint32_t ebp;
+} __attribute__((packed));
+
+void interrupt_handler(struct cpu_state cpu, uint32_t interrupt_number, uint32_t error_code);
 
 #endif
